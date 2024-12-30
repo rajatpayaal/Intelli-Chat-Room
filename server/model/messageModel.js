9e000a191eeb72829abec070969a9a4d9ecb1a01
rajatpayaal <rajatpayal@outlook.in>
2024-12-31 00:12:07 +0530

    hostchekcode

--- server/model/messageModel.js
+++ server/model/messageModel.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 
-const messageSchema = new mongoose.Schema(
-    {
-        message : {
-            text:{
-                type: String,
-                required: true,
-            },
-        },
-            users : Array,
-            sender : {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
+// const messageSchema = new mongoose.Schema(
+//     {
+//         message : {
+//             text:{
+//                 type: String,
+//                 required: true,
+//             },
+//         },
+//             users : Array,
+//             sender : {
+//                 type: mongoose.Schema.Types.ObjectId,
+//                 ref: "User",
+//                 required: true,
+//             },
         },
         {
             timestamps : true,
